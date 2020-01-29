755a379e13600621d9b09c46c5e532c529a4993c
William Wennerström <william@optmzr.se>
2020-01-29 22:55:41 +0100

    lim: add correct limits

--- lim/play.nim
+++ lim/play.nim
@@ -28,10 +28,24 @@ proc play() =
   var win: bool = true
 
   for c in randomCards(@[
-      Card(question: "sin(x)/x", answers: @["1"]),
-      Card(question: "(x^a)ln(x) [a > 0]", answers: @["0"]),
-      Card(question: "cos(x)", answers: @["1"]),
-      Card(question: "(x^a)(e^x) [a > 0]", answers: @["0"]),
+      Card(question: "x -> 0: sin(x)/x", answers: @["1"]),
+      Card(question: "x -> 0+: (x^a)ln(x) [a > 0]", answers: @["0"]),
+      Card(question: "x -> 0: tan(x)/x", answers: @["1"]),
+      Card(question: "x -> 0: ln(x+1)/x", answers: @["1"]),
+      Card(question: "x -> 0: ((e^x)-1)/x", answers: @["1"]),
+      Card(question: "x -> 0: (1+x)^(1/x)", answers: @["e"]),
+      Card(question: "x -> 0: arcsin(x)/x", answers: @["1"]),
+      Card(question: "x -> 0: arctan(x)/x", answers: @["1"]),
+      Card(question: "x -> ∞: ln(x)/(x^a) [a > 0]", answers: @["0"]),
+      Card(question: "x -> ∞: (x^a)/(a^x) [a > 1]", answers: @["0"]),
+      Card(question: "n -> ∞: a^n [|a| < 1]", answers: @["0"]),
+      Card(question: "n -> ∞: a^n [a = 1]", answers: @["1"]),
+      Card(question: "n -> ∞: a^n [a > 1]", answers: @["∞", "undef", "undefined", "inf", "infinity"]),
+      Card(question: "n -> ∞: (n^a)/(a^n) [a > 1]", answers: @["0"]),
+      Card(question: "n -> ∞: (a^n)/(n!)", answers: @["0"]),
+      Card(question: "n -> ∞: (n!)/(n^n)", answers: @["0"]),
+      Card(question: "n -> ∞: sqrt_n(n^a)", answers: @["1"]),
+      Card(question: "n -> ∞: (1+(1/n))^n", answers: @["e"])
     ]):
 
     print_gameboard(score, win)
